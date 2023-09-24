8154d95d3bcf95a20b66a12fb2726a6905c98971
Vitor Beuren <vitorbeuren@hotmail.com>
2023-09-24 14:22:27 +0100

    again at the form

--- Resources/Script/form-to-email.php
+++ Resources/Script/form-to-email.php
@@ -1,51 +1,76 @@
 <?php
+if (isset($_POST['Email'])) {
 
-$errors = '';
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "contact@vitorbeuren.com";
+    $email_subject = "New form submissions";
 
-$myemail = 'contact@vitorbeuren.com';
+    function problem($error)
+    {
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br><br>";
+        echo $error . "<br><br>";
+        echo "Please go back and fix these errors.<br><br>";
+        die();
+    }
 
-if(empty($_POST['name'])  || 
-   empty($_POST['email']) || 
-   empty($_POST['subject']) || 
-   empty($_POST['message']))
-{
-    $errors .= "\n Error: all fields are required";
+    // validation expected data exists
+    if (
+        !isset($_POST['Name']) ||
+        !isset($_POST['Email']) ||
+        !isset($_POST['Message'])
+    ) {
+        problem('We are sorry, but there appears to be a problem with the form you submitted.');
+    }
+
+    $name = $_POST['name']; // required
+    $email = $_POST['email']; // required
+    $message = $_POST['message']; // required
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+    if (!preg_match($email_exp, $email)) {
+        $error_message .= 'The Email address you entered does not appear to be valid.<br>';
+    }
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+    if (!preg_match($string_exp, $name)) {
+        $error_message .= 'The Name you entered does not appear to be valid.<br>';
+    }
+
+    if (strlen($message) < 2) {
+        $error_message .= 'The Message you entered do not appear to be valid.<br>';
+    }
+
+    if (strlen($error_message) > 0) {
+        problem($error_message);
+    }
+
+    $email_message = "Form details below.\n\n";
+
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
+    }
+
+    $email_message .= "Name: " . clean_string($name) . "\n";
+    $email_message .= "Email: " . clean_string($email) . "\n";
+    $email_message .= "Message: " . clean_string($message) . "\n";
+
+    // create email headers
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
+?>
+
+    <!-- include your success message below -->
+
+    Thank you for contacting us. We will be in touch with you very soon.
+
+<?php
 }
-
-$name = $_POST['name']; 
-$email_address = $_POST['email']; 
-$subject = $_POST['subject']; 
-$message = $_POST['message']; 
-
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
-}
-
-if( empty($errors))
-
-{
-
-$to = $myemail;
-
-$email_subject = "Contact form submission: $subject , $name";
-
-$email_body = "You have received a new message. ".
-
-" Here are the details:\n Name: $name \n ".
-
-"Email: $email_address\n Subject: $subject\n Message \n $message";
-
-$headers = "From: $myemail\n";
-
-$headers .= "Reply-To: $email_address";
-
-mail($to,$email_subject,$email_body,$headers);
-
-//redirect to the 'thank you' page
-
-header('Location: contact-form-thank-you.html');
-
-}
+?>
